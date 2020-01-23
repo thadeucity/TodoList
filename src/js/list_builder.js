f29ac4a8ebbbc7f0c0c8b92a476f942423e47d59
Victor <thadeucity@gmail.com>
2020-01-23 11:13:35 -0300

    Basic Implementation of Adding and Deleting Tasks

---
+++ src/js/list_builder.js
@@ -0,0 +1,72 @@
+var listElement = document.querySelector('#list ul');
+
+var inputTask = document.querySelector('#todo');
+var inputTime = document.querySelector('#time');
+
+var btnSaveTask = document.querySelector('#save-task');
+
+// var taksList = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+var taskList = JSON.parse(localStorage.getItem('list_tasks')) || [];
+
+function renderTasks(){
+  listElement.innerHTML = '';
+
+  for(todo of taskList) {
+      var taskElement = document.createElement('li');
+      var taskTitle = document.createElement('strong');
+      var taskHour = document.createElement('span');
+      var btnDeleteTask = document.createElement('button');
+
+      var todoText = document.createTextNode(todo.task);
+      var todoTime = document.createTextNode(todo.time);
+      var btnText = document.createTextNode('X');
+
+      var pos = taskList.indexOf(todo);
+
+      taskElement.setAttribute('class', 'task');
+      btnDeleteTask.setAttribute('onclick', `deleteTask(${pos})`);
+
+      taskTitle.appendChild(todoText);
+      taskHour.appendChild(todoTime);
+      btnDeleteTask.appendChild(btnText);
+      
+      taskElement.appendChild(taskTitle);
+      taskElement.appendChild(taskHour);
+      taskElement.appendChild(btnDeleteTask);
+
+      listElement.appendChild(taskElement);
+  }
+}
+
+function addTask(){
+  var taskText = inputTask.value;
+  var taskTime = inputTime.value;
+
+  var taskInfo = {
+    task: taskText,
+    time: taskTime
+  };
+
+  if (taskText != ''){
+      taskList.push(taskInfo);
+      inputTask.value = '';
+      inputTime.value = '';
+      renderTasks();
+      saveToStorage();
+  }
+}
+
+function deleteTask(pos){
+  taskList.splice(pos, 1);
+  renderTasks();
+  saveToStorage();
+}
+
+function saveToStorage(){
+  localStorage.setItem('list_tasks', JSON.stringify(taskList));
+}
+
+btnSaveTask.onclick = addTask;
+
+renderTasks();
